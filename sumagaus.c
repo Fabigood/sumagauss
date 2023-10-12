c8decc1a1c02b106048201c02f12e1bd95104573
Fabigood <fabio.gonzalez.fiallos@udla.edu.ec>
2023-10-12 16:22:36 -0500

    for

--- sumagaus.c
+++ sumagaus.c
@@ -5,13 +5,14 @@ int main(){
     printf("Ingrese un numero entero: ");
     scanf("%d", &num );
 
-    if (num < 1);
+    if (num < 1) {
         printf("Numero no valido ingrese un numero valido .\n");
+        }else{  
+        for (int i = 1; i<= num; i++) {
+            sum += i;
+        }
 
-
-
-
-
-
+        printf("El resultado de %d es esto  %d\n", num , sum);
+    }
     return 0;
 }
